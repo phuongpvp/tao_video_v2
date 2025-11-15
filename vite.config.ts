5117df9ea494683926e2faa93a3d51d534193caa
phuongpv <phuong@thienphuads.com>
2025-11-15 22:51:31 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       // BẮT BUỘC THÊM DÒNG NÀY VÀO
-      base: './', 
+      base: '/', 
 
       server: {
         port: 3000,
